dfcb504928b3a0db684aa85c6faa6ef0d45771b7
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 08:58:34 +0100

    [GREEN] Change 139

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -4,19 +4,12 @@ module PathFinding
   def find_path(graph, start, finish)
     return {true, [start]} if start == finish
 
-    nodes = graph.fetch(start, empty)
-
-    i = 0
-    while nodes.size > i
-      node = nodes[i]
-
+    graph.fetch(start, empty).each do |node|
       return {true, [start, finish]} if node == finish
 
       if graph.fetch(node, empty).includes?(finish)
         return {true, [start, node, finish]}
       end
-
-      i += 1
     end
 
     {false, empty}
