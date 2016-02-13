7df9f66c7c3a19e04ce051d7b2519346b2958004
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 09:03:25 +0100

    [GREEN] Change 149

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -1,11 +1,12 @@
 module PathFinding
   extend self
 
-  def find_path(graph, start, finish)
+  def find_path(graph, start, finish, visited=empty)
+    return {false, empty} if visited.includes?(start)
     return {true, [start]} if start == finish
 
     graph.fetch(start, empty).each do |node|
-      ok, path = find_path(graph, node, finish)
+      ok, path = find_path(graph, node, finish, visited + [start])
       return {true, [start] + path} if ok
     end
 
