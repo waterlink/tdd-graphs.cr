bcec3ea2ddf2b6f6d05c9e239a127fc13da95f3f
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 08:47:12 +0100

    [RED] Change 115

--- spec/path_finding_spec.cr
+++ spec/path_finding_spec.cr
@@ -2,4 +2,9 @@ require "./spec_helper"
 require "../src/path_finding"
 
 describe PathFinding do
+  empty_graph = {} of Int32 => Array(Int32)
+
+  it "finds a path" do
+    PathFinding.find_path(empty_graph, 1, 2)
+  end
 end
