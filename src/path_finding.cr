5a805f806da36d96457654571c18f740e9f30038
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 08:57:00 +0100

    [GREEN] Change 136

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -11,21 +11,14 @@ module PathFinding
     nodes = graph.fetch(start, empty)
 
     i = 0
-    if nodes.size > i
+    while nodes.size > i
       node = nodes[i]
 
       if graph.fetch(node, empty).includes?(finish)
         return {true, [start, node, finish]}
       end
-    end
 
-    i = 1
-    if nodes.size > i
-      node = nodes[i]
-
-      if graph.fetch(node, empty).includes?(finish)
-        return {true, [start, node, finish]}
-      end
+      i += 1
     end
 
     {false, empty}
