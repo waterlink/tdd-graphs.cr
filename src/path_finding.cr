16c6eaefd2863ff3eca43cc6aea1bcf892294e31
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 08:50:33 +0100

    [GREEN] Change 122

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -4,6 +4,12 @@ module PathFinding
   def find_path(graph, start, finish)
     return {true, [start]} if start == finish
 
+    if graph.has_key?(start)
+      if graph[start].first == finish
+        return {true, [start, finish]}
+      end
+    end
+
     {false, empty}
   end
 
