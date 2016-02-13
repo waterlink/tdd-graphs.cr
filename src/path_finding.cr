662f3f88feae69cd9594c744511884b372c486c1
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 09:00:40 +0100

    [RED] Change 143

--- src/path_finding.cr
+++ src/path_finding.cr
@@ -8,7 +8,7 @@ module PathFinding
       return {true, [start, finish]} if node == finish
 
       graph.fetch(node, empty).each do |node2|
-        return {true, [start, node, node2, finish]} if node == finish
+        return {true, [start, node, finish]} if node2 == finish
       end
     end
 
