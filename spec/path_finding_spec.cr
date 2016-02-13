ee884fa62277f6665ecf2c6998fdf92a659ddc53
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-13 08:52:35 +0100

    [RED] Change 127

--- spec/path_finding_spec.cr
+++ spec/path_finding_spec.cr
@@ -21,7 +21,7 @@ describe PathFinding do
     PathFinding.find_path({1 => [3, 2]}, 1, 2)
       .should eq({true, [1, 2]})
 
-    PathFinding.find_path({1 => [3, 2], 2 => [5]}, 1, 5)
+    PathFinding.find_path({1 => [2], 2 => [5]}, 1, 5)
       .should eq({true, [1, 2, 5]})
   end
 end
